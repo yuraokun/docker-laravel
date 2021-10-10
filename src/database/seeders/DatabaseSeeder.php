fefc9381b524b2a15a1818d656dc0e78f221bf71
honda yuki <yuraokun0@gmail.com>
2021-10-10 16:38:55 +0900

    seeders implemented

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+         $tags = \App\Models\Tag::factory(10)->create();
+        //  \App\Models\Listing::factory(10)->create();
+
+        \App\Models\User::factory(20)->create()->each(function($user) use($tags){
+            \App\Models\Listing::factory(rand(1, 4))->create([
+                    'user_id' => $user->id
+            ])->each(function($listing) use($tags) {
+                $listing->tags()->attach($tags->random(rand(1, 2)));
+            });
+        });
     }
 }
